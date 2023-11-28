6509b72a6016165589c56f8a8c89e60a6701cbf4
acherkanov <135016183+acherkanov@users.noreply.github.com>
2023-11-28 14:25:25 +0300

    UPDATE

--- android/src/main/java/com/rncamerakit/QRCodeAnalyzer.kt
+++ android/src/main/java/com/rncamerakit/QRCodeAnalyzer.kt
@@ -6,7 +6,7 @@ import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
 import com.google.mlkit.vision.barcode.BarcodeScannerOptions
 import com.google.mlkit.vision.barcode.BarcodeScanning
-import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.common.InputImage
 
 class QRCodeAnalyzer (
