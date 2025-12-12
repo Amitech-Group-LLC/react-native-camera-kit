e1bdf28b243aaec023d40afbf8965e0f34f3b470
Kseniya Vinnichek <ms.vinnichek@outlook.com>
2025-12-12 14:08:34 +0100

    removed OnCameraShowEvent from oldarch

--- android/src/oldarch/java/com/rncamerakit/CKCameraManager.kt
+++ android/src/oldarch/java/com/rncamerakit/CKCameraManager.kt
@@ -51,7 +51,6 @@ class CKCameraManager(var context: ReactApplicationContext) : SimpleViewManager<
             OrientationChangeEvent.EVENT_NAME, MapBuilder.of("registrationName", "onOrientationChange"),
             ReadCodeEvent.EVENT_NAME, MapBuilder.of("registrationName", "onReadCode"),
             PictureTakenEvent.EVENT_NAME, MapBuilder.of("registrationName", "onPictureTaken"),
-            OnCameraShowEvent.EVENT_NAME, MapBuilder.of("registrationName", "onCameraShow"),
             ZoomEvent.EVENT_NAME, MapBuilder.of("registrationName", "onZoom"),
             ErrorEvent.EVENT_NAME, MapBuilder.of("registrationName", "onError"),
             CaptureButtonPressInEvent.EVENT_NAME, MapBuilder.of("registrationName", "onCaptureButtonPressIn"),
